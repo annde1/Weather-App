f3dc9b5ce320d71e5e8aafd1e5c21f4abf313d1e
annde1 <annadepta61@gmail.com>
2023-08-28 13:48:02 +0300

    Layout changes

--- Script/app.js
+++ Script/app.js
@@ -5,7 +5,6 @@ import { getDate } from "./date.js";
 import { updateDate } from "./date.js";
 
 const API_KEY = "7a57e0eb094bf63e8e3fef9c28de69bd";
-
 const searchInput = document.querySelector("#search-input");
 let weather;
 
@@ -13,41 +12,45 @@ let weather;
 displayTime();
 setInterval(displayTime, 1000);
 let obj = getDate();
-console.log(obj);
 updateDate(obj);
 
+const fetchAndUpdateWeather = async (city) => {
+  weather = new Weather(API_KEY, city);
+  try {
+    let data = await weather.fetchWeatherByCity();
+
+    let mainData = data.main;
+    let cityName = data.name;
+    let weatherDescription = data.weather;
+    let wind = data.wind.speed;
+    const { description, icon } = weatherDescription[0];
+    let { feels_like, humidity, pressure, temp, temp_max, temp_min } = mainData;
+
+    updateWeatherData(
+      cityName,
+      Math.floor(temp),
+      Math.floor(feels_like),
+      Math.floor(temp_min),
+      Math.floor(temp_max),
+      humidity,
+      pressure,
+      wind,
+      description,
+      icon
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+//TODO: When page is loaded display current weather for default city
+let defaulCity = encodeURIComponent("New York");
+weather = new Weather(API_KEY, defaulCity);
+fetchAndUpdateWeather(defaulCity);
+
+//When user hits enter on the keyboard he will get weather by the city name he chose
 searchInput.addEventListener("keydown", async (e) => {
   if (e.key === "Enter") {
     const city = encodeURIComponent(searchInput.value);
-    console.log(city);
-    weather = new Weather(API_KEY, city);
-    console.log(weather);
-    try {
-      let data = await weather.fetchWeatherByCity();
-
-      let mainData = data.main;
-      let cityName = data.name;
-      let weatherDescription = data.weather;
-      let wind = data.wind.speed;
-      const { description, icon } = weatherDescription[0];
-      console.log(description);
-      let { feels_like, humidity, pressure, temp, temp_max, temp_min } =
-        mainData;
-      console.log(temp);
-      updateWeatherData(
-        cityName,
-        Math.floor(temp),
-        Math.floor(feels_like),
-        Math.floor(temp_min),
-        Math.floor(temp_max),
-        humidity,
-        pressure,
-        wind,
-        description,
-        icon
-      );
-    } catch (err) {
-      console.log(err);
-    }
+    fetchAndUpdateWeather(city);
   }
 });
